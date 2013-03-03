c0706d1962ff6298b69143b0f8b9338231f7dc40
Adriel Paredes <adriel.paredes@gmail.com>
2013-03-02 23:10:52 -0300

    Issue #4. Close. Se termina la configuracion y se sube una explicacion de como ejecutar la app

--- openmusic-node/src/main/scala/ar/com/blout/openmusic/node/Node.scala
+++ openmusic-node/src/main/scala/ar/com/blout/openmusic/node/Node.scala
@@ -6,23 +6,51 @@ import org.jboss.resteasy.spi.ResteasyDeployment
 import org.jboss.resteasy.plugins.server.netty.NettyJaxrsServer
 import java.util.Collections
 import ar.com.blout.openmusic.node.metadata.Metadata
+import com.beust.jcommander.JCommander
+import ar.com.blout.openmusic.node.configuration.CLIParameters
+import ar.com.blout.openmusic.node.configuration.CLIParameters
+import ar.com.blout.openmusic.node.configuration.Configuration
 
 object Node {
 
-  val logger = LogManager.getLogger(this.getClass())
-  val port = 8080
+  var logger = LogManager.getLogger(this.getClass())
 
   def main(args: Array[String]): Unit = {
 
     logger info "Comienza el proceso"
-    var deployment = new ResteasyDeployment
-    deployment.setResourceClasses(Collections.singletonList(classOf[SongService].getName()))
+    configure(args)
+    netty(Configuration getInt "port", deployment) start
 
+  }
+
+  /*
+   * AUXILIARES
+   */
+
+  /**
+   * Crea el servidor netty que permite escuchar en un puerto especificado las peticiones rest
+   */
+  def netty(port: Int, deployment: ResteasyDeployment): NettyJaxrsServer = {
     var netty = new NettyJaxrsServer
     netty setPort port
     netty setDeployment deployment
-    netty start
-    
+    return netty
+  }
+
+  /**
+   * Genera las clases que devuelven las especificaciones de los servicios rest
+   */
+  def deployment: ResteasyDeployment = {
+    var deployment = new ResteasyDeployment()
+    deployment.setResourceClasses(Collections.singletonList(classOf[SongService].getName()))
+    return deployment
+  }
+
+  def configure(args: Array[String]) = {
+    var cli = new CLIParameters
+    new JCommander(cli, args.toArray: _*)
+    logger info cli.folder
+    Configuration config cli.folder
   }
 
 }
